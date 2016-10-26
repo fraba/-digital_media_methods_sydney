1a7f6d6f85fee12745458f4a16fbf2157696dcd4
Francesco Bailo <me@francescobailo.net>
2016-10-26 14:54:22 +1100

    Added R code

---
+++ ws01/code/ws01_code.R
@@ -0,0 +1,9 @@
+# Load the data
+## The following line will read the data (a comma separated values (CSV) file) with the function 
+## `read.csv()` and will store it in the variable `my_data`.
+my_data <- read.csv('https://raw.githubusercontent.com/fraba/digital_media_methods_sydney/master/ws01/data/twitter_data.csv')
+
+# View the data
+## If you are working in RStudio, you might check the data with the function View().
+View(my_data)
+
