ee5ba6e9b176a6f7b3ebe7ba4f294a922a5e45e0
Francesco Bailo <me@francescobailo.net>
2016-11-11 10:37:12 +1100

    Remove line path to local file

--- ws02/code/ws02_code.R
+++ ws02/code/ws02_code.R
@@ -4,8 +4,6 @@ install.packages('twitterR')
 # Load the library
 library(twitteR)
 
-source('~/public_git/referendum_ita_2016/twt_local_info_not_for_git.R')
-
 # Replace square brakets and what contained in them with your credentials
 ## To obtain your keys see: https://fraba.github.io/digital_media_methods_sydney/ws02/ws02.html#get-twitter-api
 twitter_auth_dict =
@@ -18,5 +16,5 @@ twitter_auth_dict =
 setup_twitter_oauth(twitter_auth_dict[['consumer_key']], twitter_auth_dict[['consumer_secret']],
                     twitter_auth_dict[['access_token']], twitter_auth_dict[['access_token_secret']])
 
-# Seacrh something
+# Search something
 searchTwitter('pizza')
